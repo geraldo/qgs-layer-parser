889a1734105bf33299f10900451c106fe937ecbe
geraldo <geraldo@servus.at>
2020-01-16 14:34:03 +0100

    add option for directly loading wms layers without using qgis nor mapproxy

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # qgs-layer-parser
-Parse QGIS 3 project file and create JSON config file
 
-Start the script using `/usr/bin/python3.5 -b parseQGS.py [file.qgs]`
+Parse QGIS 3 project files and write a JSON config file with layer information.
 
-Creates a JSON configuration file called `[file.json]` in the same directory.
+## Getting started
 
-The project includes a test QGIS 3 project called [bcn-geodata.qgs](https://github.com/geraldo/qgs-layer-parser/blob/master/bcn-geodata.qgs) and it's geojson sources. It uses [Open Data from Barceclona](https://github.com/martgnz/bcn-geodata) with information about districts and neighbourhoods.
+Start the script using `python3 parseQGS.py project.qgs`
 
-A typical output for a layer could look like that:
+It creates a JSON configuration file called `[project.qgs.json]`.
+
+A typical output for a layer could look like this:
 
 ```json
   {
@@ -34,6 +35,23 @@ A typical output for a layer could look like that:
   },
 ```
 
+## Configuration
+
+- `project_path` defines the path to the local project directory where .qgs file is located.
+- `dest_path' defines the path to the output directory.
+
+## Special characters
+
+- `@` to hide layers: You can use character `@` in front of layer/group names in QGIS to set `"hidden": true`. This tells layerswitcher to avoid showing layer/group but renders layer/groups by default.
+- `~` to hide legend: You can use character `~` in front of layer/group names in QGIS to set `"showLegend": true`. This can be used to avoid rendering the legend.
+- `¬` to load WMS layers directly: You can use character `¬` in front of layer/group names in QGIS to set `"external": true`. This option also looks up layer source and writes the following parameters:
+  - `"wmsUrl": ''`
+  - `"wmsLayers": ''`
+  - `"wmsProjection": ''`
+
+## More info
+
+The project includes a test QGIS 3 project called [bcn-geodata.qgs](https://github.com/geraldo/qgs-layer-parser/blob/master/bcn-geodata.qgs) and it's geojson sources. It uses [Open Data from Barceclona](https://github.com/martgnz/bcn-geodata) with information about districts and neighbourhoods.
+
 It also includes an exmaple using [ol-layerswitcher](https://github.com/walkermatt/ol-layerswitcher) using the produced [bcn-geodata.json](https://github.com/geraldo/qgs-layer-parser/blob/master/bcn-geodata.json) file to configure layers. You can see the result opening [layerswitcher.html](https://go.yuri.at/infovis/ol-layerswitcher/layerswitcher.html).
 
-You can use character `@` in front of layer/group names in QGIS to set `"hidden": true`. This tells layerswitcher to avoid showing layer/group but renders layer/groups by default.
--- parseQGS.py
+++ parseQGS.py
@@ -5,123 +5,160 @@ import json
 import unicodedata
 import sys
 
-project = 'ctbb'
-project_path = '/home/gerald/Documents/PSIG/ctbb/parse/'
-#project_file = 'poum.qgs'
-#project_file = 'guia'
-#project_file = 'ortofotos_historial'
-#project_file = 'activitats'
+def run():
+	project_path = '/var/servers/ctbb/maps/'
+	dest_path = '/var/servers/ctbb/web/js/data/'
+	# project_path = '/home/gerald/Documents/PSIG/ctbb/maps/'
+	# dest_path = '/var/www/html/CTBB/js/data/'
 
-prj_file = sys.argv[1]
-project_file = prj_file.replace('.qgs', '')
+	prj_file = sys.argv[1]
+	project_file = prj_file.replace('.qgs', '')
 
-# create a reference to the QgsApplication, setting the
-# second argument to False disables the GUI
-qgs = QgsApplication([], False)
+	# create a reference to the QgsApplication, setting the
+	# second argument to False disables the GUI
+	qgs = QgsApplication([], False)
 
-# supply path to qgis install location
-qgs.setPrefixPath("/usr", True)
+	# supply path to qgis install location
+	qgs.setPrefixPath("/usr", True)
 
-# load providers
-qgs.initQgis()
+	# load providers
+	qgs.initQgis()
 
-# Get the project instance
-project = QgsProject.instance()
-gui = QgsGui.instance()
+	# Get the project instance
+	project = QgsProject.instance()
+	gui = QgsGui.instance()
 
-# Load qgis project
-if not project.read(project_path+prj_file):
-    print('Something went wrong with the project file!')
+	# Load qgis project
+	if not project.read(project_path+prj_file):
+	    print('Something went wrong with the project file!')
+	    print('You have to call the script adding the absolute path of the project file like: /home/qgis/project.qgs')
 
-print("Project file:", project.fileName())
-print("Project title: ", project.title())
+	print("Project title:", project.title())
+	print("Project file:", project.fileName())
 
-def replaceSpecialChar(text):
-    chars = "!\"#$%&'()*+,./:;<=>?@[\\]^`{|}~"
-    for c in chars:
-        text = text.replace(c, "")
-    return text
+	def replaceSpecialChar(text):
+	    chars = "!\"#$%&'()*+,./:;<=>?@[\\]^`{|}~"
+	    for c in chars:
+	        text = text.replace(c, "")
+	    return text
 
-def stripAccents(s):
-   return ''.join(c for c in unicodedata.normalize('NFD', s)
-                  if unicodedata.category(c) != 'Mn')
+	def stripAccents(s):
+	   return ''.join(c for c in unicodedata.normalize('NFD', s)
+	                  if unicodedata.category(c) != 'Mn')
 
-def layertree(node):
-	#print(node.dump())
-	#print(node.name(), type(node))
-	obj = {}
+	def layertree(node):
+		#print(node.dump())
+		#print(node.name(), type(node))
+		obj = {}
 
-	if isinstance(node, QgsLayerTreeLayer):
-		obj['name'] = node.name()
-		obj['mapproxy'] = project+"_"+project_file+"_layer_"+replaceSpecialChar(stripAccents(obj['name'].lower().replace(' ', '_')))
-		obj['type'] = "layer"
-		obj['indentifiable'] = node.layerId() not in nonidentify
-		obj['visible'] = node.isVisible()
-		obj['hidden'] = node.name().startswith("@")
-		if obj['hidden']:
-			obj['visible'] = True 	# hidden layers/groups have to be visible by default
-		obj['showlegend'] = not node.name().startswith("~")
-		obj['fields'] = []
-		obj['actions'] = []
-		#print("- layer: ", node.name())
-		
-		layer = project.mapLayer(node.layerId())
+		if isinstance(node, QgsLayerTreeLayer):
+			obj['name'] = node.name()
+			obj['mapproxy'] = "ctbb_"+project_file+"_layer_"+replaceSpecialChar(stripAccents(obj['name'].lower().replace(' ', '_')))
+			obj['type'] = "layer"
+			obj['indentifiable'] = node.layerId() not in nonidentify
+			obj['visible'] = node.isVisible()
+			obj['hidden'] = node.name().startswith("@")	# hide layer from layertree
+			if obj['hidden']:
+				obj['visible'] = True 	# hidden layers/groups have to be visible by default
+			obj['showlegend'] = not node.name().startswith("~") and not node.name().startswith("¬")	# don't show legend in layertree
+			obj['fields'] = []
+			obj['actions'] = []
+			obj['external'] = node.name().startswith("¬")
 
-		#aliases = layer.attributeAliases()
+			# fetch layer directly from external server (not from QGIS nor mapproxy)
+			if obj['external']:
+				obj['name'] = node.name()[1:]
+				src = project.mapLayer(node.layerId()).source()
+				
+				# wms url
+				istart = src.index("url=")+4
+				try:
+					iend = src.index("&", istart)
+				except ValueError:
+					iend = len(src)
+				obj['wmsUrl'] = src[istart:iend]
+				
+				# wms layers
+				istart = src.index("layers=")+7
+				try:
+					iend = src.index("&", istart)
+				except ValueError:
+					iend = len(src)
+				obj['wmsLayers'] = src[istart:iend]
+				
+				# wms srs
+				istart = src.index("crs=")+4
+				try:
+					iend = src.index("&", istart)
+				except ValueError:
+					iend = len(src)
+				obj['wmsProjection'] = src[istart:iend]
 
-		if obj['indentifiable'] and isinstance(layer, QgsVectorLayer):
-			for index in layer.attributeList():
-				if layer.editorWidgetSetup(index).type() != 'Hidden':
-					#print(layer.fields()[index].name(), layer.attributeDisplayName(index))
+			#print("- layer: ", node.name())
 
-					f = {}
-					#f['name'] = layer.fields()[index].name()
-					#f['alias'] = layer.attributeDisplayName(index)
-					f['name'] = layer.attributeDisplayName(index)
-					obj['fields'].append(f)
+			layer = project.mapLayer(node.layerId())
 
-			for action in gui.mapLayerActionRegistry().mapLayerActions(layer):
-				a = {}
-				a['name'] = action.name()
-				a['action'] = action.action()
-				obj['actions'].append(a)
-		
+			if obj['indentifiable'] and isinstance(layer, QgsVectorLayer):
+
+				fields = []
+
+				# get all fields like arranged using the Drag and drop designer
+				edit_form_config = layer.editFormConfig()
+				root_container = edit_form_config.invisibleRootContainer()
+				for field_editor in root_container.findElements(QgsAttributeEditorElement.AeTypeField):
+					i = field_editor.idx()
+					if i >= 0 and layer.editorWidgetSetup(i).type() != 'Hidden':
+						#print(i, field_editor.name(), layer.fields()[i].name(), layer.attributeDisplayName(i))
+
+						f = {}
+						f['name'] = layer.attributeDisplayName(i)
+						obj['fields'].append(f)
+
+				for action in gui.mapLayerActionRegistry().mapLayerActions(layer):
+					a = {}
+					a['name'] = action.name()
+					a['action'] = action.action()
+					obj['actions'].append(a)
+
+			return obj
+
+		elif isinstance(node, QgsLayerTreeGroup):
+			obj['name'] = node.name()
+			obj['mapproxy'] = "ctbb_"+project_file+"_group_"+replaceSpecialChar(stripAccents(obj['name'].lower().replace(' ', '_')))
+			obj['type'] = "group"
+			obj['visible'] = node.isVisible()
+			obj['hidden'] = node.name().startswith("@")
+			if obj['hidden']:
+				obj['visible'] = True 	# hidden layers/groups have to be visible by default
+			obj['children'] = []
+			#print("- group: ", node.name())
+			#print(node.children())
+
+			for child in node.children():
+				obj['children'].append(layertree(child))
+
 		return obj
 
-	elif isinstance(node, QgsLayerTreeGroup):
-		obj['name'] = node.name()
-		obj['mapproxy'] = project+"_"+project_file+"_group_"+replaceSpecialChar(stripAccents(obj['name'].lower().replace(' ', '_')))
-		obj['type'] = "group"
-		obj['visible'] = node.isVisible()
-		obj['hidden'] = node.name().startswith("@")
-		if obj['hidden']:
-			obj['visible'] = True 	# hidden layers/groups have to be visible by default
-		obj['children'] = []
-		#print("- group: ", node.name())
-		#print(node.children())
+	info=[]
+	nonidentify = project.nonIdentifiableLayers()
+	root = project.layerTreeRoot()
+	for group in root.children():
+		obj = layertree(group)
+		info.append(obj)
 
-		for child in node.children():
-			obj['children'].append(layertree(child))
+	# identifiable <Identify>
+	#print(project.readEntry("qgis", "Identify"))
 
-	return obj
+	# write to json file
+	f=open(dest_path+prj_file+'.json', 'w+')
+	f.write(json.dumps(info))
+	f.close()
+	#QgsVectorFileWriter.writeAsVectorFormat(i,dataStore + os.sep + 'exp_' + safeLayerName + '.js', 'utf-8', exp_crs, 'GeoJSON', layerOptions=['COORDINATE_PRECISION=3'])
 
-info=[]
-# print("Project tree:")
-nonidentify = project.nonIdentifiableLayers()
-root = project.layerTreeRoot()
-for group in root.children():
-	obj = layertree(group)
-	info.append(obj)
+	# When your script is complete, call exitQgis() to remove the
+	# provider and layer registries from memory
+	qgs.exitQgis()
 
-# identifiable <Identify>
-#print(project.readEntry("qgis", "Identify"))
+	print("Layer tree written to:", dest_path+prj_file+'.json')
 
-# write to json file
-f=open(prj_file+'.json', 'w+')
-f.write(json.dumps(info))
-f.close()
-#QgsVectorFileWriter.writeAsVectorFormat(i,dataStore + os.sep + 'exp_' + safeLayerName + '.js', 'utf-8', exp_crs, 'GeoJSON', layerOptions=['COORDINATE_PRECISION=3'])
-
-# When your script is complete, call exitQgis() to remove the
-# provider and layer registries from memory
-qgs.exitQgis()
+run()
